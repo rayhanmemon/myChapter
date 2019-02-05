a7c89923518ad6812dfc8fc8a3d916b0ab1154d5
rayhanmemon <rayhanmemon@gmail.com>
2019-02-04 20:31:49 -0500

    PUSH NOTIFICATIONS NOW WORK
    push notifications are now operational!

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,41 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.sendPushNotification = functions.database.ref('/{organization}/posts/{pushId}/')
+var fetch = require('node-fetch');
+
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.sendPushNotification = functions.database.ref('/{organization}/posts/{postId}/')
     .onCreate((snapshot, context) => {
-      // Grab the current value of what was written to the Realtime Database.
+      const organization = context.params.organization;
+      const organizationRef = snapshot.ref.root.child(`/${organization}`);
+
+      const postId = context.params.postId;
       const post = snapshot.val();
-      console.log('sendPushNotification', context.params, post);
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return snapshot.ref.parent.child('function').set('worked!');
+
+      let messages = [];
+
+      return organizationRef.child('/profiles').once('value').then(snapshot => {
+        snapshot.forEach(childSnapshot => {
+          let pushToken = childSnapshot.val().pushToken;
+          if (pushToken) {
+            messages.push({
+              "to": pushToken,
+              "title" : post.name,
+              "body": post.postContent
+            })
+          }
+        });
+        return Promise.all(messages);
+      }).then(messages => {
+        return fetch('https://exp.host/--/api/v2/push/send', {
+          method: "POST",
+          headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(messages)
+      });
     });
-
-
-//postContent = snapshot.val().postContent
-//
+  });
